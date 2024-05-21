59902256f74e33b48542065e244593d791d81f75
Aleš Kučera <kuceral4@fel.cvut.cz>
2024-05-21 09:23:50 +0200

    Update

--- scripts/traversability.py
+++ scripts/traversability.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+import struct
 from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs import point_cloud2
 from std_msgs.msg import Header
 
 
+def unpack_rgb(rgb_float):
+    """Unpack a 32-bit packed float into RGB components."""
+    s = struct.pack('>f', rgb_float)
+    i = struct.unpack('>I', s)[0]
+    r = (i >> 16) & 0x0000ff
+    g = (i >> 8) & 0x0000ff
+    b = i & 0x0000ff
+    return r, g, b
+
+
 def process_point_cloud(point_cloud_msg: PointCloud2):
     rospy.logdebug(f"Fields: {point_cloud_msg.fields}")
     # Extract points from the incoming point cloud message
-    points = point_cloud2.read_points(point_cloud_msg, field_names=("x", "y", "z", "r", "g", "b"), skip_nans=True)
+    points = point_cloud2.read_points(point_cloud_msg, field_names=("x", "y", "z", "rgb"), skip_nans=True)
 
     processed_points = []
     for point in points:
-        x, y, z, r, g, b = point
+        x, y, z, rgb_float = point
+        r, g, b = unpack_rgb(rgb_float)
         if r == 0:
             cost = 0.5
         else:
@@ -40,9 +52,11 @@ def process_point_cloud(point_cloud_msg: PointCloud2):
 
     return filtered_cloud_msg
 
+
 def point_cloud_callback(msg: PointCloud2):
     filtered_cloud_msg = process_point_cloud(msg)
     pub.publish(filtered_cloud_msg)
+
 
 if __name__ == '__main__':
     rospy.init_node('semantic_traversability_node', log_level=rospy.DEBUG)
